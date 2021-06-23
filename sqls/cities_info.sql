a9f841a99e551f30c09ca4f535e1f6c670fc4bb8
VenYy <58319623+VenYy@users.noreply.github.com>
2021-06-23 10:09:21 +0800

    Create cities_info.sql

---
+++ sqls/cities_info.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 8.0.25 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `cities_info` (
+	`time` varchar (60),
+	`provinceName` varchar (60),
+	`citiesName` varchar (60),
+	`citiesData` int (10)
+); 
